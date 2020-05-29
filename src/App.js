56fd03ebcadd9583d835ab9e5d1639576d226a33
Yves Siqueira <yves.as@gmail.com>
2020-05-29 16:37:50 -0300

    expo project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Header from './components/header';
+import Footer from './components/footer';
+import Home from './pages/home'
 
-function App() {
-  return (
+const App = () => (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <CssBaseline />
+        <Header/>
+        <main>
+            <Home/>
+        </main>
+        <Footer/>
     </div>
-  );
-}
+);
 
 export default App;
